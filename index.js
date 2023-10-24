f1729f1ca3384481a900f8b73f5d721ccf310c8f
Nathan Chen <113192742+seribical@users.noreply.github.com>
2023-10-24 13:15:55 -0400

    dddd

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-/* const DOMSelectors={
-    button:document.getElementById("btn"),
-    text:document.querySelector("#text"),
-    box: document.getElementById("big-black-box"),
-    points: document.querySelectorAll(".point"),
-};
-
-function backgroundAndText(background,text){
-    background.style.backgroundColor = "red";
-    text.textContent = "This is now red die";
-    text.style.fontSize = "40px"
-}
-
-
-DOMSelectors.button.addEventListener("click",function(){
-    backgroundAndText(DOMSelectors.box,DOMSelectors.text);
-});
-
-function changeLi(){
-    let pointIndex = 1;
-    DOMSelectors.points.forEach((point)=>{
-        point.addEventListener("click",function(){
-            point.textContent = `Hello i am point ${pointIndex}`;
-            pointIndex++;
-        });
-    });
-}
-changeLi(); */
-
 const DOMSelectors = {
-    button:document.getElementById("btn"),
-    box:document.getElementById("container-box"),
-    input:document.querySelector(`#input`),
-}
+    button: document.getElementById("btn"),
+    box: document.getElementById("flexbox"),
+    cardtitle: document.querySelector(`#cardtitle`),
+    carddesc: document.querySelector(`#carddesc`),
+    cardimg: document.querySelector(`#cardimg`)
+  };
+  
+  DOMSelectors.button.addEventListener("click", function () {
+    let cardtitle = DOMSelectors.cardtitle.value; // .value lets you get the values of an element, in this case something you typed in
+    let carddesc = DOMSelectors.carddesc.value;
+    let cardimg = DOMSelectors.cardimg.value;
+  
+    console.log(cardtitle,carddesc,cardimg);
+  
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+        `
+          <h2 class = '??'>${cardtitle}</h2>
+          <p class = '/? '>${carddesc}</p>
+          <img src='${cardimg}'>
+        `
+    );
+  }); // this function will cause new HTML to be added when the input button is clicked.
+  
+  DOMSelectors.button.insertAdjacentHTML(
+    "afterend",
+    ``
+  );
+  // you can put multiple HTML tags in the same DOMSelector as if you were writing out regular HTML, you're also able to assigned classes and ids that can be used for styling the elements you add through js.
+  
