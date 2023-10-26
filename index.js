65958c836447e36a7bdd6d4d1ec8708912325f66
Nathan Chen <113192742+seribical@users.noreply.github.com>
2023-10-26 13:12:39 -0400

    asdfghjkl

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 const DOMSelectors = {
     button: document.getElementById("btn"),
+    button2: document.getElementById("rbtn"),
     box: document.getElementById("flexbox"),
     cardtitle: document.querySelector(`#cardtitle`),
     carddesc: document.querySelector(`#carddesc`),
     cardimg: document.querySelector(`#cardimg`)
-  };
-  
-  DOMSelectors.button.addEventListener("click", function () {
-    let cardtitle = DOMSelectors.cardtitle.value;
-    let carddesc = DOMSelectors.carddesc.value;
-    let cardimg = DOMSelectors.cardimg.value;
-  
-    console.log(cardtitle,carddesc,cardimg);
-  
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-        `
-        <div id="card">
-        <h2 class = '??'>${cardtitle}</h2>
-        <img src='${cardimg}' class="img">
-        <p class = '/? '>${carddesc}</p>
-    </div>
-        `
-    );
-  });
+};
+
+DOMSelectors.button.addEventListener("click", function (event) {
+  event.preventDefault;
+  data();
+});
+
+function data(){
+  const album = {
+    cardtitle: DOMSelectors.cardtitle.value,
+    carddesc: DOMSelectors.carddesc.value,
+    cardimg: DOMSelectors.cardimg.value,
+  }
+  inject(album);
+  clearentry();
+};
+
+function inject(album){
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+      `
+      <div id="card">
+      <h2 class = '??'>${album.cardtitle}</h2>
+      <img src='${album.cardimg}' class="img">
+      <p class = '/? '>${album.carddesc}</p>
+  </div>
+      `
+)};
+
+function clearentry(){
+  DOMSelectors.cardtitle.value = '';
+  DOMSelectors.carddesc.value = '';
+  DOMSelectors.cardimg.value = '';
+};
+
+DOMSelectors.button2.addEventListener("click", function () {
+  remove()
+});
+
+function remove(){
+  console.log('piss')
+}
